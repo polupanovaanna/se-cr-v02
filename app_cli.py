6efc607ed98978546a1c1d84762d14339bfa1daa
Artem Zakharenko <zakhart1234567890@gmail.com>
2022-10-19 15:23:40 +0300

    Add names to game

--- app_cli.py
+++ app_cli.py
@@ -4,13 +4,14 @@ from main import *
 possible_rates = ["VoisinsDeZero", "Tier", "Orphelins", "Parity 1", "Parity 0", "Color R", "Color G", "Color B"]
 
 rate_list = []
+names = []
 dilet_rate = None
 
 while True:
-    s = input("Print new player rate:").strip()
-    if s == "No new players":
+    name = input("Print new player name:").strip()
+    if name == "No new players":
         break
-    if s == "Diler":
+    if name == "Diler":
         if dilet_rate is not None:
             print("Diler is already in the game")
             continue
@@ -18,18 +19,18 @@ while True:
         print("Diler rate is " + diler_rate_str)
         dilet_rate = convert_string_to_rate(diler_rate_str)
         continue
+    s = input("Print new player rate:").strip()
     rate_list.append(convert_string_to_rate(s))
+    names.append(name)
 
 if dilet_rate is not None:
     rate_list.append(dilet_rate)
+    names.append("Diler")
 results = get_random_game_result(rate_list)
 print(f"\n\n\nThe number on the roulette wheel is {results.result_number}")
 
 for i, res in enumerate(results.is_winner):
-    if i + 1 == len(rate_list) and dilet_rate is not None:
-        print("Diler", end='')
-    else:
-        print(f"{i}th player", end='')
+    print(names[i], end='')
     if results.is_winner[i]:
         print(" won the bet")
     else:
