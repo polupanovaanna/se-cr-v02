7013df0cf4109aa249db2de4aa65b387e78d3482
Artem Zakharenko <zakhart1234567890@gmail.com>
2022-11-30 13:59:14 +0300

    Some refactoring, add add_player

--- app_cli.py
+++ app_cli.py
@@ -1,11 +1,17 @@
 from main import *
-
+from dataclasses import dataclass
 
 possible_rates = ["VoisinsDeZero", "Tier", "Orphelins", "Parity 1", "Parity 0", "Color R", "Color G", "Color B"]
 
-rate_list = []
-names = []
-dilet_rate = None
+@dataclass
+class Player:
+    name: str
+    rate: RateType
+
+players: List[Player] = []
+
+def add_player(name, rate):
+    players.append(Player(name, convert_string_to_rate(rate)))
 
 def get_rate():
     str = input("Print new player rate:").strip()
@@ -16,30 +22,27 @@ def get_rate():
         if str in possible_rates:
             return str
 
+diler_rate_str = None
 while True:
     name = input("Print new player name:").strip()
     if name == "No new players":
         break
     if name == "Diler":
-        if dilet_rate is not None:
+        if diler_rate_str is not None:
             print("Diler is already in the game")
             continue
         diler_rate_str = random.choice(possible_rates)
         print("Diler rate is " + diler_rate_str)
-        dilet_rate = convert_string_to_rate(diler_rate_str)
         continue
-    s = get_rate()
-    rate_list.append(convert_string_to_rate(s))
-    names.append(name)
+    add_player(name, get_rate())
 
-if dilet_rate is not None:
-    rate_list.append(dilet_rate)
-    names.append("Diler")
-results = get_random_game_result(rate_list)
+if diler_rate_str is not None:
+    add_player("Diler", diler_rate_str)
+results = get_random_game_result([p.rate for p in players])
 print(f"\n\n\nThe number on the roulette wheel is {results.result_number}")
 
 for i, res in enumerate(results.is_winner):
-    print(names[i], end='')
+    print(players[i].name, end='')
     if results.is_winner[i]:
         print(" won the bet")
     else:
